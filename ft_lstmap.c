1670cc861fc2865b9756c74c143e1273d1a289be
psmolin <psmolin@student.42heilbronn.de>
2025-03-13 19:49:08 +0100

    lst

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: psmolin <psmolin@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/12 13:27:07 by psmolin           #+#    #+#             */
-/*   Updated: 2025/03/13 19:43:14 by psmolin          ###   ########.fr       */
+/*   Updated: 2025/03/13 19:48:33 by psmolin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,65 +36,3 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	}
 	return (ret);
 }
-
-// t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
-// {
-// 	t_list	*temp;
-// 	t_list	*ret;
-// 	t_list	*new;
-// 	void	*content;
-
-// 	if (!lst || !f || !del)
-// 		return (NULL);
-// 	ret = ft_lstnew(f(lst->content));
-// 	if (!ret)
-// 		return (NULL);
-// 	lst = lst->next;
-// 	temp = ret;
-// 	while (lst)
-// 	{
-// 		content = f(lst->content);
-// 		if (!content)
-// 			return (ft_lstclear(&ret, del), NULL);
-// 		new = ft_lstnew(content);
-// 		if (!new)
-// 			return (ft_lstclear(&ret, del), NULL);
-// 		temp->next = new;
-// 		temp = new;
-// 		lst = lst->next;
-// 	}
-// 	return (ret);
-// }
-
-// t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
-// {
-// 	t_list	*temp;
-// 	t_list	*ret;
-// 	t_list	*new;
-// 	void	*content;
-
-// 	if (!lst || !f || !del)
-// 		return (NULL);
-// 	content = f(lst->content);
-// 	if (content)
-// 		ret = ft_lstnew(content);
-// 	if (!ret)
-// 		return (NULL);
-// 	lst = lst->next;
-// 	temp = ret;
-// 	while (lst)
-// 	{
-// 		content = f(lst->content);
-// 		if (content)
-// 			new = ft_lstnew(content);
-// 		if (!new)
-// 		{
-// 			ft_lstclear(&ret, del);
-// 			return (NULL);
-// 		}
-// 		temp->next = new;
-// 		temp = new;
-// 		lst = lst->next;
-// 	}
-// 	return (ret);
-// }
